a96fdb7a3d22e28ed37cc07f535e0f3f80a33a53
Robert <112430289+robertvieiraa@users.noreply.github.com>
2026-07-04 15:08:12 -0300

    Exibi os pensamentos na página
    Corrigi também linhas da aula anterior

--- js/api.js
+++ js/api.js
@@ -2,9 +2,10 @@ const api = {
 	async buscarPensamentos() {
 		try {
 			const response = await fetch('http://localhost:3000/pensamentos')
-			return response.json()
+			return await response.json()
 		} catch {
 			alert('Erro ao buscar pensamentos.')
+			throw error
 		}
 	}
 }
