9cad6e412c3590ff5a0652fa7764a4b2c16007de
Robert <112430289+robertvieiraa@users.noreply.github.com>
2026-07-08 19:27:38 -0300

    Adicionando dados com o POST

--- js/api.js
+++ js/api.js
@@ -7,6 +7,21 @@ const api = {
 			alert('Erro ao buscar pensamentos.')
 			throw error
 		}
+	},
+	async salvarPensamento(pensamento) {
+		try {
+			const response = await fetch('http://localhost:3000/pensamentos', {
+				method: "POST",
+				headers: {
+					"Content-Type": "application/json"
+				},
+				body: JSON.stringify(pensamento)
+			})
+			return await response.json()
+		} catch {
+			alert('Erro ao buscar pensamentos.')
+			throw error
+		}
 	}
 }
 export default api;
